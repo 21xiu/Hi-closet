20a2ff92e661865829a7282f0f60ea6ecf4dd3ec
Routine Cai <823793426@qq.com>
2019-03-26 10:51:59 +0800

    second commit

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -1,20 +1,63 @@
 // pages/upload/upload.js
+var app=getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    tempFilePaths: '' ,
+    selectArray: [
+      {
+        text: "上衣",
+        value: "skirts"
+      },
+      {
+        text: "裤子",
+        value: "pants"
+      },
+      {
+        text: "裙子",
+        value: "skirt"
+      },
+      {
+        text: "外套",
+        value: "jacket"
+      },
+      {
+        text: "鞋子",
+        value: "shoes"
+      },
+      {
+        text: "袜子",
+        value: "socks"
+      },
+      {
+        text: "饰品",
+        value: "ornament"
+      }
+      ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
 
   },
-
+  chooseimage: function () {
+    var _this = this;
+    wx.chooseImage({
+      count: 1, // 默认9  
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
+      success: function(res) {
+         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
+        _this.setData({
+        tempFilePaths: res.tempFilePaths  
+        })
+      }  
+    })
+  },  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
